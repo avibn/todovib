95a168488a1fe0d2a34d3d1ca1e7b8f703855c8a
Alvis B <a.benny9@outlook.com>
2021-06-20 12:42:00 +0100

    added list view

--- src/apps/list/views/view_list.py
+++ src/apps/list/views/view_list.py
@@ -1,6 +1,12 @@
-from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.views import View
 
+from ..models import TodoList
+
+
 class ViewList(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse(f'LIST PAGE')
+    def get(self, request, list_id, *args, **kwargs):
+        todolist = get_object_or_404(TodoList, pk=list_id, owner=request.user)
+        context = {"list": todolist}
+
+        return render(request, "list/view_list.html", context)
