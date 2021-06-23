396d19427e677336350cf76acf2175ff94936455
Alvis B <a.benny9@outlook.com>
2021-06-23 16:01:05 +0100

    added sidebar

--- src/apps/list/views/view_list.py
+++ src/apps/list/views/view_list.py
@@ -7,6 +7,7 @@ from ..models import TodoList
 class ViewList(View):
     def get(self, request, list_id, *args, **kwargs):
         todolist = get_object_or_404(TodoList, pk=list_id, owner=request.user)
-        context = {"list": todolist}
+        all_lists = TodoList.objects.filter(owner=request.user)
+        context = {"list": todolist, "all_lists": all_lists}
 
         return render(request, "list/view_list.html", context)
